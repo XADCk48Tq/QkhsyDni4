d46b783a0b036409184ce9bddf966d23656e0f39
XADCk48Tq <ktmbkt924@gmail.com>
2022-09-24 20:46:08 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ const Home = () => {
     let nfts; 
     console.log("fetching nfts");
     
-    const api_key = ""
+    const api_key = "wHoHph44ltItZRNjWC2IJRdFBoWOIgUb"
 
 
 
@@ -44,7 +44,7 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = ""
+      const api_key = "wHoHph44ltItZRNjWC2IJRdFBoWOIgUb"
 
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
